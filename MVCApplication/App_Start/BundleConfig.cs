1fce68fabd9a8e245efd9a7ed0ac5f99257048e7
Fan Lin <linfan0558@gmail.com>
2019-09-08 18:09:02 -0400

    Added simple styling to home page.

--- MVCApplication/App_Start/BundleConfig.cs
+++ MVCApplication/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace MVCApplication
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
                       "~/Content/site.css"));
         }
     }
